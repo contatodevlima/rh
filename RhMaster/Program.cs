c13c02e28a66c4e2fcd1817a8d8efe2a9651006f
Rodrigo Lima <contato.devlima@gmail.com>
2020-02-29 11:01:39 -0300

    NOVO

---
+++ RhMaster/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RhMaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+        }
+    }
+}
