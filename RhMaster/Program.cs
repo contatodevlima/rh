8c775f79fdb5609133f12a8e6c2985c718e556e4
brunoco56 <61623803+brunoco56@users.noreply.github.com>
2020-02-29 11:47:38 -0300

    teste

--- RhMaster/Program.cs
+++ RhMaster/Program.cs
@@ -7,6 +7,9 @@ namespace RhMaster
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!");
+            Console.WriteLine("Hello World!!");
+
         }
+
     }
 }
